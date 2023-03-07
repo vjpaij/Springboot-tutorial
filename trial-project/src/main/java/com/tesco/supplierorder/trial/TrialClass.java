498d8a0d58ba82608d0526a100884984040e95d0
Vijay Pai <vijay.pai@tesco.com>
2023-03-07 19:33:32 +0530

    First deployment

---
+++ trial-project/src/main/java/com/tesco/supplierorder/trial/TrialClass.java
@@ -0,0 +1,9 @@
+package com.tesco.supplierorder.trial;
+
+import java.util.Arrays;
+
+public class TrialClass {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+}
