9a1338b9313da31b729a943745f3b090c7f24938
Vijay Pai <vijay.pai@tesco.com>
2023-03-08 12:25:34 +0530

    First amendments

--- trial-project/src/main/java/com/tesco/supplierorder/trial/TrialClass.java
+++ trial-project/src/main/java/com/tesco/supplierorder/trial/TrialClass.java
@@ -1,7 +1,5 @@
 package com.tesco.supplierorder.trial;
 
-import java.util.Arrays;
-
 public class TrialClass {
     public static void main(String[] args) {
         System.out.println("Hello World!!!");
